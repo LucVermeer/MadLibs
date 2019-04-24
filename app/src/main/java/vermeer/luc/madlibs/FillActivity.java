545075b54092fd291fee332fa80286cdf2704dca
Luc Vermeer <luc@legalthings.com>
2019-04-24 14:49:29 +0200

    Switch to first activity done

---
+++ app/src/main/java/vermeer/luc/madlibs/FillActivity.java
@@ -0,0 +1,15 @@
+package vermeer.luc.madlibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fill);
+    }
+
+
+}
--- app/src/main/java/vermeer/luc/madlibs/MainActivity.java
+++ app/src/main/java/vermeer/luc/madlibs/MainActivity.java
@@ -1,7 +1,10 @@
 package vermeer.luc.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(MainActivity.this, FillActivity.class);
+        startActivity(intent);
     }
 }
